d2b50c7debb795a3e3242ca11a2d8836493cad9a
kylyn.luterte <kylyn.l@shakewell.agency>
2024-11-29 13:05:48 +0800

    86cx5z50v | fix cover photo generator

--- src/Features/Packages/Actions/PDFPageParsers/CoverPhotoAction.php
+++ src/Features/Packages/Actions/PDFPageParsers/CoverPhotoAction.php
@@ -4,14 +4,54 @@ namespace Shakewellagency\ContentPortalPdfParser\Features\Packages\Actions\PDFPa
 
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+use Shakewellagency\ContentPortalPdfParser\Features\Packages\Actions\PackageInitializes\GetS3ParserFileTempAction;
 
 class CoverPhotoAction
 {
-    public function execute($renditionPage, $rendition)
-    {
-        if ($renditionPage->page !== 1) {
+    public function execute(
+        $renditionPage, 
+        $rendition, 
+        $package
+    ) {
+
+        if ($renditionPage->page_no !== 1) {
             return;
         }
 
+        $parserFile = (new GetS3ParserFileTempAction)->execute($package);
+
+        $tempHtmlPath = tempnam(sys_get_temp_dir(), "image_");
+        $imagePath = $tempHtmlPath . '.jpg';
+        rename($tempHtmlPath, $imagePath);
+
+        $command = sprintf(
+            'magick -density 300 %s[0] -resize 100%% %s', 
+            escapeshellarg($parserFile),
+            escapeshellarg($imagePath)
+        );
+
+        exec($command, $output, $returnVar);
+
+        if ($returnVar !== 0) {
+            // Log the error for debugging
+            Log::error('magick command failed', [
+                'command' => $command,
+                'output' => $output,
+                'returnVar' => $returnVar,
+            ]);
+        }
+
+        $s3FilePath = "{$package->hash}/assets/cover-photo.png";
+
+        Storage::disk('s3temp')->put($s3FilePath, file_get_contents($imagePath));
+  
+        $rendition->cover_photo_path = $s3FilePath;
+        $rendition->save();
+
+        unlink($imagePath);
+
+        return $rendition;
     }
 }
