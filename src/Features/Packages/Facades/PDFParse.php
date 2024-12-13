2106194bb155de6e3846be1fb6383eda8fe2c6f4
kylyn.luterte <kylyn.l@shakewell.agency>
2024-12-13 10:22:32 +0800

    86cxajt81 | optimize the Job dispatch

--- src/Features/Packages/Facades/PDFParse.php
+++ src/Features/Packages/Facades/PDFParse.php
@@ -5,6 +5,8 @@ namespace Shakewellagency\ContentPortalPdfParser\Features\Packages\Facades;
 use Carbon\Carbon;
 use Shakewellagency\ContentPortalPdfParser\Events\ParsingStartedEvent;
 use Shakewellagency\ContentPortalPdfParser\Events\ParsingTriggerEvent;
+use Shakewellagency\ContentPortalPdfParser\Features\Packages\Actions\PackageInitializes\GetS3ParserFileTempAction;
+use Shakewellagency\ContentPortalPdfParser\Features\Packages\Actions\PackageInitializes\UnlinkTempFileAction;
 use Shakewellagency\ContentPortalPdfParser\Features\Packages\Jobs\PackageInitializationJob;
 use Shakewellagency\ContentPortalPdfParser\Features\Packages\Jobs\PageParserJob;
 
@@ -12,9 +14,16 @@ class PDFParse
 {
     public static function execute($package, $version)
     {
+        //TODO: remove this
+        (new UnlinkTempFileAction)->execute();
+        //---- remove this
+
         event(new ParsingTriggerEvent($package, $version));
+
+        $parserFile = (new GetS3ParserFileTempAction)->execute($package);
+        
         PackageInitializationJob::withChain([
-            new PageParserJob($package, $version),
-        ])->dispatch($package, $version);
+            new PageParserJob($package, $version, $parserFile),
+        ])->dispatch($package, $version, $parserFile);
     }
 }
