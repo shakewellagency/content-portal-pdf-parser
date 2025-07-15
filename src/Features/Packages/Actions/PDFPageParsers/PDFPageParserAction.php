0055b9b9939224bd68966bd5d05a4a957bfee4eb
kylyn-shakewell <kylyn.l@shakewell.agency>
2025-07-15 11:34:05 +0800

    fix debug

--- src/Features/Packages/Actions/PDFPageParsers/PDFPageParserAction.php
+++ src/Features/Packages/Actions/PDFPageParsers/PDFPageParserAction.php
@@ -44,14 +44,13 @@ class PDFPageParserAction
             if ($extension === 'html') {
                 $htmlString = file_get_contents($file);
 
-                
-
-                $htmlString = $page == 1 ? $htmlString : ContentParserHelper::removeOutline($htmlString);
-                
                 Log::warning("Debugging page:{$page}", [
                         'html' => $htmlString
                     ]
                 );
+
+                $htmlString = $page == 1 ? $htmlString : ContentParserHelper::removeOutline($htmlString);
+                
                 $renditionPage->content = json_encode($htmlString);
                 $renditionPage->is_parsed = true;
                 $renditionPage->save();
